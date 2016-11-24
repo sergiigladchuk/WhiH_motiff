7795f616ee258d960d4d0fd8758bdb69dd332a07
SG <sergiigladchuk@gmail.com>
2016-11-24 16:49:57 +0100

    R analysis started

---
+++ results/2016-11-18/res_interpret.R
@@ -0,0 +1,25 @@
+
+# all peak analysis
+fimo <- read.csv('2016-11-18/all_peaks/fimo.txt', sep='\t')
+#View(fimo)
+
+
+fimo$logs <- -log(fimo$p.value)
+# add matches from MCAST
+
+mcast <- read.csv('2016-11-18/all_peaks/mcast.txt', sep='\t')
+#View(mcast)
+
+
+fimo$mcast <- match(fimo$start,mcast$start)
+
+# add matches from Centrimo
+
+
+
+#plot all results
+
+
+plot(fimo$logs, type = 'l', main="-log of p-value vs. index", 
+     ylab = "-log of p-value", xlab = "# of matches")
+points(fimo$mcast, col='red')
