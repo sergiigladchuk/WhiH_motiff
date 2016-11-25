677da7644f7f03158e996e58d820065f21727599
SG <sergiigladchuk@gmail.com>
2016-11-25 23:25:56 +0100

    Graphs added to notebook

--- results/2016-11-18/res_interpret.R
+++ results/2016-11-18/res_interpret.R
@@ -15,6 +15,25 @@ fimo$mcast <- match(fimo$start,mcast$start)
 fimo$mcast[!is.na(fimo$mcast)] <- fimo$logs[!is.na(fimo$mcast)]
 
 # add matches from Centrimo
+centrimo <- read.csv('2016-11-18/all_peaks/centrimo.txt')
+
+fimo$centrimo_full_match <- c(NA)
+totMathes <- 0
+rangeExtantion <- 100
+for (count1 in 1:length(centrimo$Centrimo_Reg_Matches)) {
+  for (count2 in 1:length(fimo$logs)) {
+    if (centrimo$Centrimo_Reg_Matches[count1] >= (fimo$start[count2] - rangeExtantion)
+        && centrimo$Centrimo_Reg_Matches[count1] <= (fimo$stop[count2] + rangeExtantion)){
+      fimo$centrimo_full_match[count2] = fimo$logs[count2]
+      
+      totMathes = totMathes + 1
+      break;
+    }
+  }
+
+}
+cat(totMathes, "CentriMo regions in all FIMO out of", length(centrimo$Centrimo_Reg_Matches), 
+    "with", rangeExtantion ,"range extention value")
 
 
 
@@ -22,5 +41,7 @@ fimo$mcast[!is.na(fimo$mcast)] <- fimo$logs[!is.na(fimo$mcast)]
 
 
 plot(fimo$logs, type = 'l', main="-log of p-value vs. index", 
-     ylab = "-log of p-value", xlab = "# of matches")
+     ylab = "-log of p-value", xlab = "index in FIMO")
 points(fimo$mcast, col='red')
+points(fimo$centrimo_full_match, col='blue',pch=3)
+legend(x='topright',legend = c('FIMO','MCAST','CentriMo'), lty=c(1,0,0),pch = c(NA,1,3), col=c('black','red','blue'))
